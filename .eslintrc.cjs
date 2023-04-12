4bc4fe450e741dd5529a236a97989a84745904d4
Mark Timothy Advento <95460588+markadv@users.noreply.github.com>
2023-04-12 21:49:20 +0800

    Lint and prettier changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,5 +10,17 @@ module.exports = {
   },
   extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
   plugins: [],
-  rules: {},
+  rules: {
+    'one-var': ['off'],
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', 'always-multiline'],
+    quotes: [
+      'error',
+      'single',
+      {
+        allowTemplateLiterals: true,
+      },
+    ],
+    'no-unused-expressions': ['off'],
+  },
 }
--- app.vue
+++ app.vue
@@ -1,8 +1,8 @@
 <template>
-  <div :class="{ 'dark': darkMode}">
+  <div :class="{ dark: darkMode }">
     <h1 class="text-red-500 dark:text-dim-600">Hello World!</h1>
   </div>
 </template>
 <script setup>
-  const darkMode = ref(false)
+const darkMode = ref(false)
 </script>
