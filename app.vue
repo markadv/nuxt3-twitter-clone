a5815bcca1f7cc9bb70fdeffc4294ac3417079cf
Mark Timothy Advento <95460588+markadv@users.noreply.github.com>
2023-04-12 21:22:35 +0800

    Add lint and prettier and fix it

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <h1 class="text-red-500">Hello World!</h1>
+  <div :class="{ 'dark': darkMode}">
+    <h1 class="text-red-500 dark:text-dim-600">Hello World!</h1>
   </div>
 </template>
+<script setup>
+  const darkMode = ref(false)
+</script>
